d9770872df7ba642bb8cffa02f0f1889d2fc9e2e
vinaycharlie01 <vinaykumaree5@gmail.com>
2023-02-08 13:31:50 +0530

    init

--- Array/main.go
+++ Array/main.go
@@ -18,32 +18,3 @@ func intersection(nums1 []int, nums2 []int) []int {
 	}
 	return a
 }
-
-// func main() {
-// 	Userinput := [][][]int{{{1, 2, 2, 1}, {2, 2}}, {{4, 9, 5}, {9, 4, 9, 8, 4}}, {{1, 2, 3, 2, 4, 5, 6, 7, 8, 9}, {1}}, {{1}, {0}}}
-// 	Output := [][]int{{2}, {4, 9}, {1}, {}}
-// 	var a [][]int
-// 	for _, v := range Userinput {
-// 		Result := intersection(v[0], v[1])
-// 		a = append(a, Result)
-// 	}
-// 	fmt.Println(a)
-
-// 	var map2 = map[int]int{}
-// 	for _, v := range a {
-// 		for _, val := range v {
-// 			map2[val]++
-// 		}
-// 	}
-// 	var map3 = map[int]int{}
-// 	for _, v := range Output {
-// 		for _, val := range v {
-// 			map3[val]++
-// 		}
-// 	}
-
-// 	fmt.Println(map2, map3)
-// 	// a := map[int]int{1: 10, 0: 30, 3: 30}
-// 	// b := map[int]int{0: 30, 3: 30, 1: 10}
-// 	// fmt.Println(reflect.DeepEqual(a, b))
-// }
