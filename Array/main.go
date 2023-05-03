a35e4a2fba37cc837a38212487570a20da9d6af5
vinaycharlie01 <vinaykumaree5@gmail.com>
2023-05-03 13:07:01 +0530

    maxprodcut

--- Array/main.go
+++ Array/main.go
@@ -5,6 +5,7 @@ import (
 	"math"
 	"sort"
 	"strconv"
+	"strings"
 	"sync"
 )
 
@@ -595,7 +596,29 @@ func IsWinner(player1 []int, player2 []int) int {
 	}
 }
 
+func maxProduct(words []string) {
+	var a1 []int
+	for i := 0; i < len(words); i++ {
+		for j := len(words) - 1; j >= i; j-- {
+			if !strings.ContainsAny(words[i], words[j]) {
+				a1 = append(a1, len(words[i])*len(words[j]))
+			}
+		}
+	}
+	fmt.Println(a1)
+	sort.Ints(a1)
+	if a1 != nil {
+		fmt.Println(a1[len(a1)-1])
+	} else {
+		fmt.Println(0)
+	}
+}
+
 func main() {
+
+	// a := []string{"abcw", "baz", "foo", "bar", "xtfn", "abcdef"}
+	// maxProduct(a)
+	// fmt.Println(strings.ContainsAny(a[0], a[4]))
 	// a := [][]int{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}}
 
 	// rotate2(a)
