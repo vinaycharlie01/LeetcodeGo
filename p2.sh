4536d8841f3375bdba7a4432c10f15475073278c
vinaycharlie01 <vinaykumaree5@gmail.com>
2023-08-12 15:01:35 +0530

    ok

--- p2.sh
+++ p2.sh
@@ -27,6 +27,37 @@ echo "Hello, world!"
 hello_world 7
 
 hello
+hello_world 7
+
+
+hello wol1 8
+
+
+echo "Hello, world!"
+
+hello_world
+
+
+
+hello_world 2
+
+
+hello_world 3
+work 3
+
+
+hello_world 3
+
+hello_world 4
+
+
+hello_world 5
+
+
+hello_world 6
+
+
+hello
 <<<<<<< HEAD
 echo "Hello, world!"
 hello_world 7
@@ -64,3 +95,9 @@ hello_world 7
 
 hello
 hello_world 7
+
+
+hello wol1 8
+
+
+hello wol1 8
