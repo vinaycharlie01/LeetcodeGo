ea5f776a088e3efc370987ca462604cc685f7454
vinaycharlie01 <vinaykumaree5@gmail.com>
2023-08-12 12:23:04 +0530

    2810. Faulty Keyboard added new string problem leetcode

--- strings2/main.go
+++ strings2/main.go
@@ -18,3 +18,22 @@ func SplitWordsBySeparator(words []string, separator byte) []string {
 	}
 	return b
 }
+
+func Reverse(a string) string {
+	b := []byte(a)
+	for i, j := 0, len(b)-1; i < j; i, j = i+1, j-1 {
+		b[i], b[j] = b[j], b[i]
+	}
+	return string(b)
+}
+
+func FinalString(s string) (a string) {
+	for _, v := range s {
+		if v == 'i' {
+			a = Reverse(a)
+		} else {
+			a += string(v)
+		}
+	}
+	return a
+}
