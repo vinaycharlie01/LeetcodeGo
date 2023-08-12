5ffc5a695db669110ffdaf13dea9dd1b0aa9a36e
vinaycharlie01 <vinaykumaree5@gmail.com>
2023-08-12 14:51:04 +0530

    om

--- p2.sh
+++ p2.sh
@@ -32,3 +32,37 @@ hello_world 7
 
 hello
 
+
+
+
+echo "Hello, world!"
+
+hello_world
+
+
+
+hello_world 2
+
+
+hello_world 3
+work 3
+
+
+hello_world 3
+
+hello_world 4
+
+
+hello_world 5
+
+
+hello_world 6
+
+echo "Hello, world!"
+hello_world 7
+
+hello
+hello_world 7
+
+
+hello wol1 8
