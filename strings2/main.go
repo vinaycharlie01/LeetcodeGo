d98a1c17cfa0b64228ca99073058ff1a3ac591db
VinayKumar V A <106230026+vinaycharlie01@users.noreply.github.com>
2023-12-17 12:15:21 +0530

    Update main.go

--- strings2/main.go
+++ strings2/main.go
@@ -29,6 +29,24 @@ func Reverse(a string) string {
 	return string(b)
 }
 
+
+func minLength(s string) int {
+	b := []byte(s)
+	var a bool
+	for i := 0; i < len(b)-1; i++ {
+		if b[i] == 'A' && b[i+1] == 'B' || b[i] == 'C' && b[i+1] == 'D' {
+			a = true
+			b = append(b[:i], b[i+2:]...)
+		}
+	}
+	if a == true {
+		return minLength(string(b))
+	} else {
+		return len(b)
+	}
+}
+
+
 func FinalString(s string) (a string) {
 	for _, v := range s {
 		if v == 'i' {
